a0542feb4007d6d067fcd751c62ba1ca38f68fce
IJustinChan <154855318+IJustinChan@users.noreply.github.com>
2025-04-15 22:01:42 -0600

    allowed player to move and created gravity

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Game():
     def __init__(self):
         self.__window = Window("Platformer", 750, 600, 60)
         self.__level = 1
-        # self.__player =
+        self.__player = Player(3, 50, 50, 5)
         self.__enemy_list = []
         self.__platform_list = []
         self.__star_list = []
@@ -45,18 +45,29 @@ class Game():
         title_text = Text("Jumping Shooter", "Arial", 36)
         print(title_text.get_width())
 
+        self.__player.set_pos(100, 100)
+
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
 
+            keys_pressed = pygame.key.get_pressed()
+
+            self.__player.move_x(keys_pressed)
+            self.__player.apply_gravity()
+
+            if keys_pressed[pygame.K_w] == 1 or keys_pressed[pygame.K_UP] == 1: # We will need to add extra condition we are doing double jumps (check jump count)
+                self.__player.jump() # Not created yet
+            
+            if keys_pressed[pygame.K_SPACE] == 1:
+                self.__player.shoot() # Not created yet
 
             self.__window.clear_screen()
 
-
-            # self.__window.get_surface().blit(title_text.get_surface(), title_text.get_pos())
             self.__window.get_surface().blit(title_text.get_surface(), (self.__window.get_width()/2 - title_text.get_width()/2, self.__window.get_height()/2 - title_text.get_height()/2))
+            self.__window.get_surface().blit(self.__player.get_surface(), self.__player.get_pos())
 
 
             self.__window.update_frame()
--- player.py
+++ player.py
@@ -9,22 +9,59 @@ class Player(Sprite):
         self._SURFACE.fill(self._color)
         self.__lives = lives
         self.__speed_x = speed_x
+        self.__speed_y = 0
         self.__fall_count = 0
         self.__hit = False
         self.__bullet_list = []
+        self.__direction_facing = "right"
+        self.__gravity_val = 1
 
     # --- Methods ---
+    def apply_gravity(self):
+        self.__fall_count += 1 # Add one to keep track of how long the player has been falling for
+
+        # Increase the y-speed by a certain value with 1 being the maximum (ensures it doesn't accelerate too fast)
+        # self.__fall_count is how long the player has been falling for but it is in frames per second (everytime the loop runs, we increase self.__fall_count by one)
+        # We convert self.__fall_count to seconds by dividing by the FPS, which is 60. This value tells us how long the player has been in the air for in seconds.
+        # We then multiply by gravity's constant, which can be any number. Default is 1, but a larger value means larger gravity.
+
+        self.__speed_y += min(1, (self.__fall_count/60)*self.__gravity_val)
+        # self.__speed_y += (self.__fall_count/60)*self.__gravity_val # Could also just use this if you want player to have unlimited acceleration
+
+        current_position = self.get_pos()
+        y_pos = current_position[1]
+
+        y_pos += self.__speed_y
+        self.set_pos(current_position[0], y_pos)
+
     def move_x(self, pressed_keys):
+        current_position = self.get_pos()
+        x_pos = current_position[0]
+
         if pressed_keys[pygame.K_d] == 1: # Move right
-            self.__x += self.__speed_x
+            x_pos += self.__speed_x
+            self.__direction = "right"
 
         if pressed_keys[pygame.K_a] == 1:
-            self.__x -= self.__speed_x
+            x_pos -= self.__speed_x
+            self.__direction = "left"
 
-        self.__pos = (self.__x, self.__y)
+        self.set_pos(x_pos, current_position[1])
 
     def lose_life(self):
         self.__lives -= 1
+
+    def jump(self):
+        pass
+
+    def shoot(self):
+        pass
+
+    def landed(self):
+        pass
+
+    def hit_head(self):
+        pass
 
     # --- Accessors ---
     def get_lives(self):
