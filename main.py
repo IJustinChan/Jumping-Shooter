a9c217bdbeff177243434433e2bad008e7e47e00
IJustinChan <154855318+IJustinChan@users.noreply.github.com>
2025-04-15 23:37:31 -0600

    added camera scrolling

--- main.py
+++ main.py
@@ -43,11 +43,19 @@ class Game():
     # --- Main program code ---
     def run(self):
         title_text = Text("Jumping Shooter", "Arial", 36)
-        print(title_text.get_width())
 
-        self.__player.set_pos(100, 100)
+        self.__player.set_pos(400, 200)
+
+        # --- Variables to control how the camera moves as the player moves ---
+        scroll_x = 0
+        scroll_area_width = 100
+        scroll_y = 0
+        scroll_area_bottom = 100
+        scroll_area_top = 200
+
 
         while True:
+            # --- INPUTS ---
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -55,8 +63,9 @@ class Game():
 
             keys_pressed = pygame.key.get_pressed()
 
+            # --- PROCESSING ---
             self.__player.move_x(keys_pressed)
-            self.__player.apply_gravity()
+            # self.__player.apply_gravity()
 
             if keys_pressed[pygame.K_w] == 1 or keys_pressed[pygame.K_UP] == 1: # We will need to add extra condition we are doing double jumps (check jump count)
                 self.__player.jump() # Not created yet
@@ -64,10 +73,37 @@ class Game():
             if keys_pressed[pygame.K_SPACE] == 1:
                 self.__player.shoot() # Not created yet
 
+
+
+            # --- Handle camera movement (make camera scroll according to how the player moves) ---
+            if (self.__player.get_pos()[0] + self.__player.get_width() - scroll_x >= self.__window.get_width() - scroll_area_width) and self.__player.get_moving_right() is True:
+                scroll_x += self.__player.get_speed_x()
+
+            if (self.__player.get_pos()[0] - scroll_x <= scroll_area_width) and self.__player.get_moving_left() is True:
+                scroll_x -= self.__player.get_speed_x()
+
+            if (self.__player.get_pos()[1] - scroll_y <= scroll_area_top) and self.__player.get_speed_y() <= 0: # Player is jumping up
+                if self.__player.get_speed_y() != 0:
+                    scroll_y -= self.__player.get_speed_y()
+                else:
+                    scroll_y -= 5
+
+            if (self.__player.get_pos()[1] + self.__player.get_height() - scroll_y >= self.__window.get_height() - scroll_area_bottom) and self.__player.get_speed_y() > 0: # Player is falling
+                scroll_y += self.__player.get_speed_y()
+
+            if scroll_y > 0:
+                scroll_y = 0
+
+            # --- OUTPUTS ---
             self.__window.clear_screen()
 
-            self.__window.get_surface().blit(title_text.get_surface(), (self.__window.get_width()/2 - title_text.get_width()/2, self.__window.get_height()/2 - title_text.get_height()/2))
-            self.__window.get_surface().blit(self.__player.get_surface(), self.__player.get_pos())
+            # Don't delete the code below as it is the original one without any scrolling
+            # self.__window.get_surface().blit(title_text.get_surface(), (self.__window.get_width()/2 - title_text.get_width()/2, self.__window.get_height()/2 - title_text.get_height()/2))
+
+            # This text below includes scrolling to help with testing purposes
+            self.__window.get_surface().blit(title_text.get_surface(), (self.__window.get_width()/2 - title_text.get_width()/2 - scroll_x, self.__window.get_height()/2 - title_text.get_height()/2 - scroll_y))
+
+            self.__window.get_surface().blit(self.__player.get_surface(), (self.__player.get_pos()[0] - scroll_x, self.__player.get_pos()[1] - scroll_y))
 
 
             self.__window.update_frame()
--- player.py
+++ player.py
@@ -13,8 +13,12 @@ class Player(Sprite):
         self.__fall_count = 0
         self.__hit = False
         self.__bullet_list = []
-        self.__direction_facing = "right"
+        self.__direction_facing = "right" # Allows us to determine which way to shoot the bullet
         self.__gravity_val = 1
+
+        # bool values to see if player is moving --> helps with camera movement
+        self.__moving_right = False
+        self.__moving_left = False
 
     # --- Methods ---
     def apply_gravity(self):
@@ -38,13 +42,19 @@ class Player(Sprite):
         current_position = self.get_pos()
         x_pos = current_position[0]
 
-        if pressed_keys[pygame.K_d] == 1: # Move right
+        if pressed_keys[pygame.K_d] == 1 or pressed_keys[pygame.K_RIGHT] == 1: # Move right
             x_pos += self.__speed_x
             self.__direction = "right"
+            self.__moving_right = True
+        else:
+            self.__moving_right = False
 
-        if pressed_keys[pygame.K_a] == 1:
+        if pressed_keys[pygame.K_a] == 1 or pressed_keys[pygame.K_LEFT]: # Move left
             x_pos -= self.__speed_x
             self.__direction = "left"
+            self.__moving_left = True
+        else:
+            self.__moving_left = False
 
         self.set_pos(x_pos, current_position[1])
 
@@ -66,18 +76,20 @@ class Player(Sprite):
     # --- Accessors ---
     def get_lives(self):
         return self.__lives
+    
+    def get_speed_x(self):
+        return self.__speed_x
+    
+    def get_speed_y(self):
+        return self.__speed_y
 
-    def jump(self):
-        pass
+    def get_moving_right(self):
+        return self.__moving_right
+    
+    def get_moving_left(self):
+        return self.__moving_left
 
-    def shoot(self):
-        pass
 
-    def landed(self):
-        pass
-
-    def hit_head(self):
-        pass
 
 
 
